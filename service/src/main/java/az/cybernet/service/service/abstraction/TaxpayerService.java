18457cc74cce4aa6b6de1c75897dd2441090b4f1
elnur.ahmadov <elnur.ahmadov@cybernet.az>
2025-09-03 11:50:23 +0400

    Add service

---
+++ service/src/main/java/az/cybernet/service/service/abstraction/TaxpayerService.java
@@ -0,0 +1,4 @@
+package az.cybernet.service.service.abstraction;
+
+public interface TaxpayerService {
+}
---
+++ service/src/main/java/az/cybernet/service/service/concrete/TaxpayerServiceHandler.java
@@ -0,0 +1,10 @@
+package az.cybernet.service.service.concrete;
+
+import az.cybernet.service.service.abstraction.TaxpayerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TaxpayerServiceHandler implements TaxpayerService {
+}
